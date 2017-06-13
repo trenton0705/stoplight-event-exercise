f595d2d699c28f380e3607ce4297bc7ad1544af0
Trenton Wuerker <trenton.wuerker@gmail.com>
2017-06-13 11:43:51 -0600

    finished assignment

--- index.js
+++ index.js
@@ -2,4 +2,36 @@
   'use strict';
 
   // YOUR CODE HERE
+  var stopButton = document.querySelector('#stopButton')
+  var stopLight = document.querySelector('#stopLight')
+
+  var slowButton = document.querySelector('#slowButton')
+  var slowLight = document.querySelector('#slowLight')
+
+  var goButton = document.querySelector('#goButton')
+  var goLight = document.querySelector('#goLight')
+
+  stopButton.addEventListener('click', function(){
+    if (stopLight.classList.contains('stop')) {
+      stopLight.classList.remove('stop')
+    } else {
+      stopLight.classList.add('stop')
+    }
+  })
+
+  slowButton.addEventListener('click', function(){
+    if (slowLight.classList.contains('slow')) {
+      slowLight.classList.remove('slow')
+    } else {
+      slowLight.classList.add('slow')
+    }
+  })
+
+  goButton.addEventListener('click', function(){
+    if (goLight.classList.contains('go')) {
+      goLight.classList.remove('go')
+    } else {
+      goLight.classList.add('go')
+    }
+  })
 })();
